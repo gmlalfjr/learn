efef0bb19791b6c1238730c929b12d8fb9dc4008
Gams <gafajri@pvg.co.id>
2024-05-18 23:03:00 +0700

    update code

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofor-little/env"
 )
 
 func main() {
+	port := goDotEnvVariable("PORT")
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -15,5 +19,17 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
-	log.Fatal(app.Listen(":4040"))
+	log.Fatal(app.Listen(":" + port))
+}
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := env.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
